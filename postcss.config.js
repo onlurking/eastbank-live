4b821594524c577f564cf5c7b77ff1d2d9fd9c6d
Diogo Felix <diogo2fex@gmail.com>
2022-07-25 20:26:51 -0300

    chore: add postcss

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+import autoprefixer from "autoprefixer";
+import postcssNesting from "postcss-nesting";
+
+const config = {
+  plugins: [autoprefixer, postcssNesting],
+};
+
+export default config;
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-
-  css: {},
+  plugins: [
+    svelte({
+      configFile: "svelte.config.js",
+    }),
+  ],
 });
